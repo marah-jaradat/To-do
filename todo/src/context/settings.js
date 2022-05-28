04d81fb3590b8eb607b953aa6cb7b6485bf77052
marah-jaradat <marahjaradat97@gmail.com>
2022-05-28 19:00:19 +0300

    adding lab 32

--- todo/src/context/settings.js
+++ todo/src/context/settings.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const SettingsContext = React.createContext();
 
 export default function Settings(props) {
   const [displaySettings, setDisplaySettings] = useState(true);
 
-  const [numberOfItems, setNumberItems] = useState(6);
+  const [numberItems, setNumberItems] = useState(6);
 
   const [sortBy, setSortBy] = useState("difficulty");
 
   const state = {
     displaySettings,
     setDisplaySettings,
-    numberOfItems,
+    numberItems,
     setNumberItems,
     sortBy,
     setSortBy,
   };
+  useEffect(() => {
+    let data = JSON.parse(localStorage.getItem("settings"));
+    if (data) {
+      setDisplaySettings(data.displaySettings);
+      setNumberItems(data.numberItems);
+      setSortBy(data.sortBy);
+    }
+  }, []);
 
   return (
     <SettingsContext.Provider value={state}>
